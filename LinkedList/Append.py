55282e9ee1258add1b62ababd686ef9ce5f673fd
aanantt <61080436+aanantt@users.noreply.github.com>
2020-02-17 00:52:44 +0530

    Update Append.py

--- LinkedList/Append.py
+++ LinkedList/Append.py
@@ -1,1 +1,29 @@
+class Node:
+    def __init__(self,value):
+        self.value=value
+        self.next = None
+class linkedList:
+    def __init__(self):
+        self.head=None
+    def add_at_last(self,node):
+        if self.head is None:
+            self.head = node
+            return
+        cur_node=self.head
+        while cur_node.next:
+            cur_node=cur_node.next
+        cur_node.next=node
+    def print(self):
+        cur_node=self.head
+        while cur_node:
+            print(cur_node.value)
+            cur_node=cur_node.next
 
+l=linkedList()
+l.add_at_last(Node(1))
+l.add_at_last(Node(2))
+l.add_at_last(Node(3))
+l.add_at_last(Node(3))
+l.add_at_last(Node(2))
+l.add_at_last(Node(1))
+l.print()
